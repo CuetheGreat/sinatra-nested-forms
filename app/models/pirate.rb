eb160ea61c021433a55a215b0aab7a3b01b7e602
Clay Stewart <c.stewart89@gmail.com>
2021-09-02 18:36:54 -0400

    Done.

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,26 @@
+
+
 class Pirate
+
+    attr_accessor :name, :weight, :height, :ships
+
+    @@pirates = []
+
+    def initialize(pirate)
+        @name = pirate[:name]
+        @weight = pirate[:weight]
+        @height = pirate[:height]
+
+        @ships = pirate[:ships].map do |ship_data|
+            ship = Ship.new(ship_data)
+            ship.pirate = self
+            ship
+        end
+        @@pirates << self
+    end
+
+    def self.all
+        @@pirates
+    end
+
 end
